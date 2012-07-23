da98970bc613f86858d66c5f4ae6e4018efb86f6
resteve <resteve@zikzakmedia.com>
2012-07-23 15:38:04 +0200

    Remove import trytond.tools header

--- sale.py
+++ sale.py
@@ -3,7 +3,6 @@
 #the full copyright notices and license terms.
 
 from trytond.model import ModelView, ModelSQL, fields
-from trytond.tools import safe_eval, datetime_strftime
 from trytond.transaction import Transaction
 from trytond.pool import Pool
 
