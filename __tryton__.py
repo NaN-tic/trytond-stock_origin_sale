cbb7eed52fa0282a1b0cbdf41229d0b653b8f880
resteve <resteve@zikzakmedia.com>
2012-07-23 13:59:50 +0200

    Fix locale ca_ES

--- __tryton__.py
+++ __tryton__.py
@@ -3,14 +3,14 @@
 #the full copyright notices and license terms.
 {
     'name': 'Stock Origin Sale',
-    'name_ca_ES': 'Orígen estoc de comanda de compra',
+    'name_ca_ES': 'Origen estoc de comanda de compra',
     'name_es_ES': 'Origen stock de pedido de venta',
     'version': '2.4.0',
     'author': 'Zikzakmedia',
     'email': 'zikzak@zikzakmedia.com',
     'website': 'http://www.zikzakmedia.com/',
     'description': '''Add origin of shipment: Sale''',
-    'description_ca_ES': '''Afegeix l'orígen del albarà: comanda de venda''',
+    'description_ca_ES': '''Afegeix l'origen del albarà: comanda de venda''',
     'description_es_ES': '''Añade el origen del albarán: pedido de venta''',
     'depends': [
         'ir',
