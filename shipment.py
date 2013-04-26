e2c168d6c2b7a733d3f119a6ee543e060d90b9a5
resteve <resteve@zikzakmedia.com>
2013-04-26 15:24:50 +0200

    pyflakes fixed

--- shipment.py
+++ shipment.py
@@ -1,10 +1,11 @@
 #This file is part stock_origin_sale module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 
 __all__ = ['ShipmentOut']
 __metaclass__ = PoolMeta
+
 
 class ShipmentOut:
     "Customer Shipment"
