75f0c68dc3788e4bfc9354ca4982d833dfd2c505
resteve <resteve@zikzakmedia.com>
2015-12-01 00:50:00 +0100

    pyflakes

--- shipment.py
+++ shipment.py
@@ -1,7 +1,6 @@
 #This file is part stock_origin_sale module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
 
 __all__ = ['ShipmentOut', 'ShipmentOutReturn']
