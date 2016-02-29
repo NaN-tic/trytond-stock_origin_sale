825e0da40484e25bf8197bce51b8274e73709e8c
resteve <resteve@zikzakmedia.com>
2016-02-29 13:47:34 +0100

    Add origin in _get_shipment_sale

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.pool import Pool
+from .sale import *
 from .shipment import *
-
 
 def register():
     Pool.register(
+        Sale,
         ShipmentOut,
         ShipmentOutReturn,
         module='stock_origin_sale', type_='model')
