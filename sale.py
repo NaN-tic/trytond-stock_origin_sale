863b49ba4aba9ff0f971b2274501b7003b3ed4cf
Raimon Esteve <raimon@nan-tic.com>
2017-06-06 22:07:29 +0200

    FIX header module name

--- sale.py
+++ sale.py
@@ -1,4 +1,4 @@
-# This file is part of the sale_weight module for Tryton.
+# This file is part of the stock_origin_sale module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import PoolMeta
